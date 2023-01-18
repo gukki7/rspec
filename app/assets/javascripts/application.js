9a1bc637a1976510f6b7e94043017d846f4af007
gukki7 <101913269+gukki7@users.noreply.github.com>
2023-01-18 09:06:59 +0000

    application.jsの編集

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,19 @@ alert(hello);
 let str1 = 'Hello';
 let str2 = 'World!!';
 alert(str1 + str2);
+
+
+// メイン部分
+let alertString;
+
+// 作成した関数を呼び出し、変数へ格納
+alertString = addString("WebCamp");
+
+//変数の中身をアラートで表示する
+alert(alertString);
+
+// 作成した関数
+function addString(strA){
+  let addStr = "Hello " + strA;
+  return addStr;
+}
